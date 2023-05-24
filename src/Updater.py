6019b05dc0ea989b9bf5eb4a77d74623dc45846e
DingXinYu <229200845@qq.com>
2023-05-24 16:48:34 +0800

    📝 修复更新时的版本号错误

--- src/Updater.py
+++ src/Updater.py
@@ -26,7 +26,7 @@ def fetch():
         repo = instance.get_repo("Jin-Cheng-Ming/ZHKU-Connector")
         # 获取项目仓库:
         if repo.get_latest_release().tag_name > current_version:
-            print('\r有可用的更新：{}'.format(current_version, repo.get_latest_release().tag_name))
+            print('\r有可用的更新：{}'.format(repo.get_latest_release().tag_name))
             print('更新内容({})：\n{}'.format(
                 "{:%Y-%m-%d}".format(repo.get_latest_release().published_at), repo.get_latest_release().body))
             print('附件：')
